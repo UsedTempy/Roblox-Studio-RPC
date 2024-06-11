70f9e9b43ab54f18b21e30ac3c94859bea356559
UsedTemplate <usedtemplate@gmail.com>
2024-06-11 20:42:06 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,9 +5,19 @@ const PORT = 3000;
 
 app.use(express.json())
 
-app.get("/UpdateRPC/:scriptName", (req, res) => {
+app.get("/setScript/:scriptName", (req, res) => {
     const scriptName = req.params.scriptName
-    
+    console.log(`${scriptName} has been opened now display in RP`)
+
+    res.status(200).json({
+        response: true
+    });
+})
+
+app.get("/clearScript", (req, res) => {
+    res.status(200).json({
+        response: true
+    });
 })
 
 app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`))
